67a7438695c5d9067da0d0a4e4c9814938bdb3dc
Elania <elania.estra@gmail.com>
2022-10-31 16:57:34 -0700

    creando test

--- depot/db/migrate/20221028233707_add_uniqueness_to_product_title.rb
+++ depot/db/migrate/20221028233707_add_uniqueness_to_product_title.rb
@@ -1,5 +1,5 @@
 class AddUniquenessToProductTitle < ActiveRecord::Migration[6.1]
   def change
-    add_index(:products, :title, unique:true)
+    add_index(:products, :title, unique: true)
   end
 end
