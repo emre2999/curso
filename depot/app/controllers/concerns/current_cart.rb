4474964d8946f25fff14180ccc71c7e727ddc181
Elania <elania.estra@gmail.com>
2022-11-03 16:54:55 -0700

    mejorando carrito, loggin y auditoria

--- depot/app/controllers/concerns/current_cart.rb
+++ depot/app/controllers/concerns/current_cart.rb
@@ -4,9 +4,9 @@ module CurrentCart
     def set_cart
         begin
             @cart = Cart.find(session[:cart_id])
-        rescue ActiveRecord::RecordNotFound
-            @cart = Cart.create
-            session[:cart_id] = @cart.id 
+            rescue ActiveRecord::RecordNotFound
+             @cart = Cart.create
+            session[:cart_id] = @cart.id
         end
     end
 end
--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  get 'store/index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
